5f1667c40c1bbeb5abfd9af38716165875957557
Mads <madsgroenbeck@gmail.com>
2026-01-06 10:55:14 +0100

    Added some files from yesterday for fun :)

---
+++ teest.py
@@ -0,0 +1,9 @@
+import numpy as np
+def add_arrays(arr1, arr2):
+    return np.add(arr1, arr2)
+
+if __name__ == "__main__":
+    a = np.array([1, 2, 3])
+    b = np.array([4, 5, 6])
+    result = add_arrays(a, b)
+    print("Succes! we fucking did it mate! Result of addition:", result)

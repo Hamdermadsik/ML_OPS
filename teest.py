0207b0200ea2cfd91e7aa8db5a53eddde59ada2a
Mads Leth Grønbeck <142385956+Hamdermadsik@users.noreply.github.com>
2026-01-06 11:59:44 +0100

    Update teest.py

--- teest.py
+++ teest.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 def add_arrays(arr1, arr2):
     return np.add(arr1, arr2)
 
@@ -7,3 +8,4 @@ if __name__ == "__main__":
     b = np.array([4, 5, 6])
     result = add_arrays(a, b)
     print("Succes! we fucking did it mate! Result of addition:", result)
+
